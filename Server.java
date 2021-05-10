9d87a8aa02a61a22ad6aa021bffa8f9d0d706400
thong1512 <71810424+thong1512@users.noreply.github.com>
2021-05-10 12:12:40 +0700

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+
+    private static final int portNumber = 4444;
+
+    private int serverPort;
+    private List<ClientThread> clients; 
+
+    public static void main(String[] args){
+        Server server = new Server(portNumber);
+        server.startServer();
+    }
+
+    public Server(int portNumber){
+        this.serverPort = portNumber;
+    }
+
+    public List<ClientThread> getClients(){
+        return clients;
+    }
+
+    private void startServer(){
+        clients = new ArrayList<ClientThread>();
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(serverPort);
+            acceptClients(serverSocket);
+        } catch (IOException e){
+            System.err.println("Could not listen on port: "+serverPort);
+            System.exit(1);
+        }
+    }
+
+    private void acceptClients(ServerSocket serverSocket){
+
+        System.out.println("server starts port = " + serverSocket.getLocalSocketAddress());
+        while(true){
+            try{
+                Socket socket = serverSocket.accept();
+                System.out.println("accepts : " + socket.getRemoteSocketAddress());
+                ClientThread client = new ClientThread(this, socket);
+                Thread thread = new Thread(client);
+                thread.start();
+                clients.add(client);
+            } catch (IOException ex){
+                System.out.println("Accept failed on : "+serverPort);
+            }
+        }
+    }
+}
